093c2f2c461438cac816c3a8735278754e11e6e8
Gopinath <gopinath.manchineella@gmail.com>
2017-11-12 14:57:09 -0500

    versioncommit

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rsync --progress -a ~/Projects/eldinero/dist/ ~/Projects/publish/mgnath.github.io/
 echo $1
-sed -i '' 's/z.z.z/"$1"/g' index.html
+sed -i '' "s/z.z.z/$1/g" index.html
 git add .
 git commit -a -m "$2"
-git push
+git push 

1de241755b597074f7a449f60d03204e97f9e540
Gopinath <gopinath.manchineella@gmail.com>
2017-11-12 14:37:19 -0500

    versioncommit

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rsync --progress -a ~/Projects/eldinero/dist/ ~/Projects/publish/mgnath.github.io/
+sed -i '' 's/z.z.z/$1/g' index.html
 git add .
-git commit -a -m "$1"
+git commit -a -m "$2"
 git push

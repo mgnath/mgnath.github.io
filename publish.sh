be5e666f8fb09704717fb396fb6a56d463789cbc
Gopinath <gopinath.manchineella@gmail.com>
2017-11-21 19:08:13 -0500

    sort fix

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-rsync --progress -a ~/Projects/eldinero/dist/ ~/Projects/publish/mgnath.github.io/
-echo $1
+rsync --progress -a -q ~/Projects/eldinero/dist/ ~/Projects/publish/mgnath.github.io/$1
+cd $1
+sed -i '' "s/z.z.z/$1/g" index.html
+cd ..
+rsync --progress -a -q ~/Projects/eldinero/dist/ ~/Projects/publish/mgnath.github.io
 sed -i '' "s/z.z.z/$1/g" index.html
 git add .
 git commit -a -m "$2"
